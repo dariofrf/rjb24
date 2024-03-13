13446cc976a4c7cca4505e59ecb9f1dcfea56943
dariofrf <dariofilipe@hotmail.com>
2024-03-13 12:22:12 +0000

    fix

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,24 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+      const cachePromises = urlsToCache.map((urlToCache) => {
+        return fetch(urlToCache, { mode: "no-cors" }) // Added no-cors mode to handle CORS policy for external resources
+          .then((response) => {
+            // Only cache successful responses
+            if (response.status === 200 || response.type === "opaque") {
+              return cache.put(urlToCache, response);
+            }
+            console.error(
+              `Skipping caching for ${urlToCache}, not a successful response`
+            );
+          })
+          .catch((error) => {
+            console.error(`Failed to fetch and cache ${urlToCache}`, error);
+          });
+      });
+      return Promise.all(cachePromises).then(() => {
+        console.log("All requested resources have been cached");
+      });
     })
   );
 });
